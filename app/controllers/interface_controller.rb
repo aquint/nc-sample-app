965111953bd5066e3eec305650e5b46a7d2ddf6e
aquint <aquinton_22@hotmail.com>
2014-11-11 00:08:27 -0500

    add tasks to interface

--- app/controllers/interface_controller.rb
+++ app/controllers/interface_controller.rb
@@ -1,12 +1,13 @@
 class InterfaceController < ApplicationController
   def show
   	@team = Team.new
-  	@teams = Team.paginate(:per_page => 20, :page => params[:page])
-  	@users = User.paginate(:per_page => 20, :page => params[:page])
+  	@teams = Team.all
+  	@users = User.all
+    @task_lists = TaskList.all
   end
   def join
   	@team = Team.find(params[:team_id])
   	@team_users = @team.users
-  	@all_users = User.paginate(:per_page => 20, :page => params[:page])
+  	@all_users = User.all
   end
 end
